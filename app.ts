e639547aabecb61869c764429c9a833292acaef6
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-09 23:52:28 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+let alien_color:string ='Green'
+
+if(alien_color === 'Green'){
+    console.log('The player just earned 5 points')
+} else if (alien_color !== 'Green') {
+    console.log("The player just earned 10 points")
+} else {
+    console.log()
+}
+
+let alienColor:string ='Red'
+
+if(alienColor === 'Green'){
+    console.log('The player just earned 5 points')
+} else if (alienColor !== 'Green') {
+    console.log("The player just earned 10 points")
+}
